2898b8eecfeb96e28010887d38f2800d2a06d0bf
mataotao <mttwind@foxmail.com>
2020-08-13 16:19:53 +0800

    new ModalPlugin()和ModalPlugin()两种方式创建实例

--- modalplugin.js
+++ modalplugin.js
@@ -1,8 +1,17 @@
 (function () {
     function ModalPlugin() {
+        return new init()
+    }
+//想使用创建对象的方式创建实例new ModalPlugin()或当做普通函数执行也能创建实例ModalPlugin(),需要这样做
 
+    //类的原型: 公共的属性方法
+    ModalPlugin.prototype = {
+        constructor: ModalPlugin
     }
 
+    function init() {}
+    init.prototype = ModalPlugin.prototype;
+    // 浏览器直接导入,这样的方法是暴露到全局的
     window.ModalPlugin = ModalPlugin;
     //如果还需要支持ES6Module/CommonJS模块导入规范,在react项目当中,vue项目当中也想用
     if (typeof module !== 'undefined' && module.exports !== 'undefined') {//如果module不存在,typeof不会出错,会返回undefined
