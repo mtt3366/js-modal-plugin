26653cfa34da8dff086ffcf1a9a11d768e76d8e0
mataotao <mttwind@foxmail.com>
2020-08-16 19:02:59 +0800

    对参数进行处理

--- modalplugin.js
+++ modalplugin.js
@@ -3,6 +3,7 @@
     function ModalPlugin(options) {
         return new init(options)
     }
+
 //想使用创建对象的方式创建实例new ModalPlugin()或当做普通函数执行也能创建实例ModalPlugin(),需要这样做
     ModalPlugin.prototype = {
         constructor: ModalPlugin,
@@ -11,23 +12,29 @@
             this.creatDom()
         },
         //创建DOM结构
-        creatDom(){
+        creatDom() {
+            let {title, template, button} = this.options
             //如果用creatElement插入DOM,每一次动态插入,都会导致DOM的回流,非常消耗性能,所以最外面使用createElement创建,内部使用字符串的方式拼写进去,创建好了之后放到最外层的容器当中,只引起一次回流
             let frag = document.createDocumentFragment()
             let dpnDialog = document.createElement('div')
             dpnDialog.className = 'dpn-dialog'
             dpnDialog.innerHTML = `
               <div class="dpn-title">
-                系统温馨提示
+                ${title}
                 <i class="dpn-close"></i>
               </div>
               <div class="dpn-content">
-            
+                ${template}
               </div>
-              <div class="dpn-handle">
-                <button>确定</button>
-                <button>取消</button>
-              </div>`
+              ${button.length > 0
+                ? `<div class="dpn-handle">
+                      ${button.map((item,index) => {
+                    return `<button index="${index}">${item.text}</button>`
+                }).join('')}
+                   </div>`
+                : ''
+            }
+              `
             frag.appendChild(dpnDialog)
 
             let dpnModel = document.createElement('div')
@@ -38,22 +45,24 @@
 
             this.dpnDialog = dpnDialog//挂载到实例上,便于其他方法的控制隐藏,并且是私有的实例,
             this.dpnModel = dpnModel
-        }
+        },
+
     }
 
     function init(options) {
         //接下来将所有的操作全部写在init里面
         //参数初始化:传递进来的配置项替换默认的配置项
         options = Object.assign({
-            title:'系统提示',
-            template:null,
-            frag:true,
-            buttons:[{}]
-        },options)
+            title: '系统提示',
+            template: null,
+            frag: true,
+            buttons: [{}]
+        }, options)
         //把信息挂载到实例上: 在原型的各个方法中,只要this是实例,都可以调用到这些信息
         this.options = options;
         this.init()
     }
+
     init.prototype = ModalPlugin.prototype;
     // 浏览器直接导入,这样的方法是暴露到全局的
     window.ModalPlugin = ModalPlugin;
@@ -66,16 +75,16 @@
 //使用:
 ModalPlugin({
     //提示的标题信息
-    title:'系统提示',
+    title: '系统提示',
     //内容模板 字符串 /模板字符串/DOM元素对象
-    template:null,
+    template: null,
     //是否开启拖拽
-    frag:true,
+    frag: true,
     //自定义按钮信息
-    buttons:[{
+    buttons: [{
         //按钮文字
-        text:'确定',
-        click(){
+        text: '确定',
+        click() {
             //this:当前实例
         }
     }]
