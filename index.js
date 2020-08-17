283e111ca988565ebbcbac58ca2addb7490e9655
mataotao <mttwind@foxmail.com>
2020-08-17 09:12:50 +0800

    使用示例,index.js

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
 !function(){
-    //基本语法
-    // let modal = ModalPlugin({
-    //     //提示的标题信息
-    //     title:'系统提示',
-    //     //内容模板 字符串 /模板字符串/DOM元素对象
-    //     template:null,
-    //     //是否开启拖拽
-    //     frag:true,
-    //     //自定义按钮信息
-    //     buttons:[{
-    //         //按钮文字
-    //         text:'确定',
-    //         click(){
-    //             //this:当前实例
-    //         }
-    //     }]
-    // })
-    // modal.open()//=>打开
-    // modal.close()//=>关闭
+//使用:
+    const modal1 = ModalPlugin({
+        //提示的标题信息
+        title: '系统提示',
+        //内容模板 字符串 /模板字符串/DOM元素对象
+        template: null,
+        //自定义按钮信息
+        buttons: [{
+            //按钮文字
+            text: '确定了',
+            click() {
+                //this:当前实例
+                this.close()
+            }
+        }, {
+            //按钮文字
+            text: '取消',
+            click() {
+                //this:当前实例
+                this.close()
+            },
 
-//基于发布订阅,实现回电函数的监听
-//     modal.on('input/open/close/dragstart/dragmove/dragend',[func])
-    // modal.off(...)
+        }]
+    })
+    modal1.on('open',()=>{
+        console.log('我被打开了1')
+    })
+    modal1.on('open',()=>{
+        console.log('我被打开了2')
+    })
+    modal1.on('close',()=>{
+        console.log('我被关闭了')
+    })
+    modal1.open()
 }()
