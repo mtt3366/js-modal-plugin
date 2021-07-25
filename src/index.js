6f6e0992b8f91ce5044e0ba939e2b15887f2bc49
马涛涛 <1030557028@qq.com>
2021-07-26 02:11:48 +0800

    1. 创建所需要的DOM结构 2. 基于事件委托绑定容器中的事件

--- src/index.js
+++ src/index.js
@@ -17,11 +17,101 @@
 import utils from './lib/utils';
 
 function ModalPlugin(config) {
-
+    let self = this;
+    self.config = config;
+    self.$drag_modal = null;
+    self.$drag_content = null;
+    self.init();
 }
 ModalPlugin.prototype = {
     version: '1.0.0',
-    constructor: ModalPlugin
+    constructor: ModalPlugin,
+    init() {
+        let self = this;
+        self.create();
+
+        // 基于事件委托实现容器中元素点击处理的相关事务：关闭按钮 && 自定义按钮
+        if (self.$drag_content) {
+            self.$drag_content.addEventListener('click', function (ev) {
+                let target = ev.target,
+                    targetTag = target.tagName,
+                    targetClass = target.className;
+                // 关闭按钮
+                if (targetTag === 'A' && targetClass === 'drag_close') {
+                    self.close();
+                    return;
+                }
+                // 自定义按钮
+                if (targetTag === 'A' && targetClass === 'drag_button') {
+                    let index = +target.getAttribute('index'),
+                        item = self.config.buttons[index];
+                    if (item && utils.isFunction(item.click)) {
+                        item.click.call(self, self);
+                    }
+                    return;
+                }
+            });
+        }
+    },
+    // 动态创建Modal的DOM结构
+    create() {
+        let self = this,
+            config = self.config,
+            fragment = document.createDocumentFragment();
+        // 创建遮罩层
+        if (config.modal) {
+            self.$drag_modal = document.createElement('div');
+            self.$drag_modal.className = 'drag_modal';
+            fragment.appendChild(self.$drag_modal);
+        }
+        // 创建内容
+        self.$drag_content = document.createElement('div');
+        self.$drag_content.className = 'drag_content';
+        self.$drag_content.innerHTML = `
+            <div class="drag_head">
+                ${config.title}
+                <a href="javascript:;" class="drag_close"></a>
+            </div>
+            <div class="drag_main">${config.template}</div>
+            ${config.buttons.length>0?`<div class="drag_foot">
+                ${config.buttons.map((item,index)=>{
+            return `<a href="javascript:;" class="drag_button" index="${index}">
+                        ${item.text}
+                    </a>`;
+        }).join('')}
+            </div>`:``}
+        `;
+        fragment.appendChild(self.$drag_content);
+
+        // 把动态创建的元素添加到页面中
+        document.body.appendChild(fragment);
+        fragment = null;
+
+        // 控制元素显示：透明度设置为1「过渡动画」
+        self.$drag_content.offsetHeight; //刷新渲染队列(获取一下样式,这样上面的添加到页面和改变元素样式就分两次执行了)
+        self.$drag_modal ? self.$drag_modal.style.opacity = 1 : null;
+        self.$drag_content.style.opacity = 1;
+    },
+    // 关闭Modal（页面中移除掉）
+    close() {
+        let self = this,
+            body = document.body;
+        if (self.$drag_modal) {
+            self.$drag_modal.style.opacity = 0;
+            self.$drag_modal.ontransitionend = () => {
+                // 元素的过渡动画结束后
+                body.removeChild(self.$drag_modal);
+                self.$drag_modal = null;
+            };
+        }
+        if (self.$drag_content) {
+            self.$drag_content.style.opacity = 0;
+            self.$drag_content.ontransitionend = () => {
+                body.removeChild(self.$drag_content);
+                self.$drag_content = null;
+            };
+        }
+    }
 };
 
 /* 暴露API */
